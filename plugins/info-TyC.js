d253daa616784b3168fd39b33b3e5bd964ca1464
xalocc <146940476+xalocc@users.noreply.github.com>
2023-11-12 11:35:03 -0500

    Update info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -27,30 +27,4 @@ _6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedi
 _- Si en algún momento recibe un mensaje del numero del Bot y no es un comando posiblemente sea de el propietario del Bot o el propietario del numero, este es un persona real por lo que se le pide que no le falte el respeto_
 
 *➤ Base usada para este Bot:*
-*- https://github.com/BochilGaming/games-wabot-md*
-
-*➤ Agradecimientos a:*
-*- Unx21*
-*- Narutomo*
-*- BochilGaming*
-*- Unxteam*
-*- Bochilteam*
-*- ConfuMods*
-*- Theh2so4*
-*- Hiro*
-*- The.sad.boy01*
-*- botstylee*
-*- Amel*
-*- FatihArridho*
-*- Adiwajshing*
-*- Darlyn*
-*- Etc.*
-
-*⁉️ Te quedaron dudas?*
-_- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_ 
-
-> wa.me/5219992095479
-> Bruno Sobrino 
-> Correo electronico: theshadowbrokers133@gmailcom
-
-*∆ THE MYSTIC - BOT - MD ∆*`;
+*- https://github.com/BochilGaming/games-wabot-md*`;
